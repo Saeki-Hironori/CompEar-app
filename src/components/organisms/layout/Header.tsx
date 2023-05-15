4bec9b29697f105e051a26378747d4805723709a
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-05-15 18:04:12 +0900

    header 検索タブ, footer Item選択タブ追加, recoil機能追加

--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,13 +1,48 @@
-import React from "react";
-import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { signOut } from "firebase/auth";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { itemsState } from "@/components/atoms/recoil/items-state";
+import { signOut } from "firebase/auth";
 import { auth } from "@/components/firebase/firebase";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Autocomplete,
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  TextField,
+  Toolbar,
+} from "@mui/material";
+import useAllItems from "@/hooks/useAllItems";
+import { Item } from "@/types/Item";
+
+const makers = [
+  "SONY",
+  "Apple",
+  "AKG",
+  "SENNHEISER",
+  "BOSE",
+  "audio-technica",
+  "SHURE",
+  "JBL",
+  "final",
+];
 
 const Header = () => {
+  const [items, setItems] = useRecoilState<Array<Item>>(itemsState);
+
   const router = useRouter();
   const user = auth.currentUser;
+  const [value, setValue] = useState<string | null>(null);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const { getItems } = useAllItems();
+
+  useEffect(() => {
+    getItems();
+  }, []);
 
   const handleLogout = () => {
     signOut(auth)
@@ -15,6 +50,29 @@ const Header = () => {
         router.push("/Login");
       })
       .catch((error) => {});
+  };
+
+  const searchMaker = (str: string) => {
+    console.log(items);
+    const searchedItems = items.filter((item) => {
+      return item.maker === str;
+    });
+    console.log(searchedItems);
+    setItems(searchedItems);
+  };
+
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter") return;
+    e.preventDefault();
+    if (value) {
+      console.log(`Valueは"${value}"`);
+      searchMaker(value);
+      setValue(null);
+    } else {
+      console.log(`inputValueは${inputValue}`);
+      searchMaker(inputValue);
+      setInputValue("");
+    }
   };
 
   return (
@@ -25,14 +83,34 @@ const Header = () => {
           edge="start"
           color="inherit"
           aria-label="menu"
-          sx={{ mr: 2 }}
+          sx={{ flex: "0.2", mr: 2 }}
         >
           <MenuIcon />
         </IconButton>
         <p style={{ flex: "1" }}>{user?.displayName || user?.email}</p>
-        <p style={{ flex: "1" }}>（{user?.uid}）</p>
-        <div style={{ flex: "5" }}></div>
-        <Button variant="outlined" color="inherit" onClick={handleLogout}>
+        <FormControl>
+          <Autocomplete
+            value={value}
+            onChange={(event: any, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            onKeyDown={handleInputKeyDown}
+            options={makers}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Maker" />}
+          />
+        </FormControl>
+        <Box sx={{ flex: "1" }}></Box>
+        <Button
+          variant="outlined"
+          color="inherit"
+          onClick={handleLogout}
+          sx={{ flex: "0.2", height: "40px" }}
+        >
           <p>Logout</p>
         </Button>
       </Toolbar>
